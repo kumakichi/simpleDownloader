8fdb9e8632db8b258be11a2e5979f314422712cc
cm_san <xbapj1@gmail.com>
2017-03-15 14:37:08 +0800

    init

---
+++ simpleDownloader.go
@@ -0,0 +1,680 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"net/url"
+	"os"
+	"path/filepath"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+
+	"errors"
+	"net/http"
+
+	"github.com/cheggaaa/pb"
+	"golang.org/x/net/proxy"
+)
+
+const (
+	appName               string = "SimpleHttp"
+	defaultOutputFileName string = "default"
+	CFG_FILENAME          string = ".sh_cfg"
+	CFG_DELIMETER         string = "##"
+	buffer_size           int    = 1024 * 1024
+)
+
+type Cookie struct {
+	Key, Val string
+}
+
+type Header struct {
+	Header, Value string
+}
+
+var (
+	tryThreadhold  int
+	connNum        int
+	userAgent      string
+	showVersion    bool
+	debug          bool
+	urls           []string
+	outputPath     string
+	outputFileName string
+	outputFile     *os.File
+	contentLength  int
+	acceptRange    bool
+	noticeDone     chan bool
+	bar            *pb.ProgressBar
+	cookiePath     string
+	usrDefHeader   string
+	forcePiece     bool
+	cfgPath        string
+	proxyAddr      string
+	dialer         proxy.Dialer
+)
+
+type SortString []string
+
+func (s SortString) Len() int {
+	return len(s)
+}
+
+func (s SortString) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s SortString) Less(i, j int) bool {
+	strI := strings.Split(s[i], ".part.")
+	strJ := strings.Split(s[j], ".part.")
+	numI, _ := strconv.Atoi(strI[1])
+	numJ, _ := strconv.Atoi(strJ[1])
+	return numI < numJ
+}
+
+func init() {
+	flag.IntVar(&tryThreadhold, "t", 5, "Retry threshold")
+	flag.IntVar(&connNum, "n", 5, "Specify the number of connections")
+	flag.StringVar(&outputFileName, "O", defaultOutputFileName, `Set output filename.`)
+	flag.StringVar(&userAgent, "U", appName, "Set user agent")
+	flag.StringVar(&cfgPath, "c", ".", "Config file path")
+	flag.BoolVar(&debug, "d", false, "Print debug infomation")
+	flag.BoolVar(&forcePiece, "f", false, "Force goaxel to use multi-thread")
+	flag.StringVar(&outputPath, "o", ".", "Set output directory.")
+	flag.BoolVar(&showVersion, "V", false, "Print version and copyright")
+	flag.StringVar(&cookiePath, "load-cookies", "", `Cookie file in the format, originally used by Netscape's cookies.txt`)
+	flag.StringVar(&usrDefHeader, "header", "", `Semicolon seperated header string`)
+	flag.StringVar(&proxyAddr, "x", "", "Set Proxy,<Protocol://HOST:PORT>")
+	proxy.RegisterDialerType("http", NewHttpProxy)
+}
+
+func downloadOnePiece(rangeFrom, pieceSize, alreadyHas int,
+	u string, c []Cookie, h []Header, ofname string) {
+	dlDone := false
+
+	f, err := os.OpenFile(ofname, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0664)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	for try := 0; try < tryThreadhold; try++ {
+		Info.Printf("try time(s):%d, rangeFrom: %d, pieceSize:%d, alreadyHas:%d\n", try, rangeFrom, pieceSize, alreadyHas)
+
+		resp, err := Get(u, c, h, rangeFrom, pieceSize, alreadyHas)
+		if err != nil {
+			Error.Println(err.Error())
+			time.Sleep(time.Second)
+			continue
+		}
+
+		written := writeContent(resp, f, pieceSize, alreadyHas)
+
+		alreadyHas += written
+		if alreadyHas == pieceSize {
+			dlDone = true
+			break
+		}
+	}
+	noticeDone <- dlDone
+}
+
+func writeContent(resp *http.Response, f *os.File, pieceSize, alreadyHas int) (written int) {
+	fileOffset := alreadyHas
+	data := make([]byte, buffer_size)
+	written = 0
+
+	defer resp.Body.Close()
+
+	var n int
+	var err error
+	for {
+		left := pieceSize - alreadyHas - written
+		if left >= buffer_size {
+			n, err = resp.Body.Read(data)
+		} else {
+			n, err = resp.Body.Read(data[:left])
+		}
+
+		if err != nil && err != io.EOF {
+			Error.Println(err.Error())
+			return
+		}
+
+		f.WriteAt(data[:n], int64(fileOffset))
+		fileOffset += n
+		written += n
+
+		bar.Add(n)
+
+		if err == io.EOF || fileOffset == pieceSize {
+			return
+		}
+	}
+}
+
+func parseUrl(urlStr string) (path string, host string, e error) {
+	if ok := strings.Contains(urlStr, "//"); ok != true {
+		urlStr = "http://" + urlStr //scheme not specified,treat it as http
+	}
+
+	path = urlStr
+
+	u, err := url.Parse(urlStr)
+	if err != nil {
+		fmt.Println("ERROR:", err.Error())
+		e = err
+		return
+	}
+
+	if u.Scheme != "http" && u.Scheme != "https" {
+		fmt.Println("xx", u.Scheme)
+		e = errors.New("only support http/https")
+		return
+	}
+
+	host = u.Host
+
+	return
+}
+
+func getChunkFilesList(outputName string) (partFiles []string, e error) {
+	err := filepath.Walk(".", func(path string, f os.FileInfo, err error) error {
+		if f == nil {
+			return err
+		}
+		if f.IsDir() {
+			return nil
+		}
+		if strings.HasPrefix(path, outputName+".part.") {
+			partFiles = append(partFiles, path)
+		}
+		return nil
+	})
+
+	if err != nil {
+		e = err
+		fmt.Printf("ERROR:", err.Error())
+		return
+	}
+	sort.Sort(SortString(partFiles))
+
+	return
+}
+
+func fileSize(fileName string) int64 {
+	if fi, err := os.Stat(fileName); err == nil {
+		return fi.Size()
+	}
+	return 0
+}
+
+func divideAndDownload(u string, cookie []Cookie, header []Header) (realConn int) {
+	var ofname string
+	var startPos, remainder int
+	realConn = connNum
+
+	Info.Printf("acceptRange:%t, connNum:%d\n", acceptRange, connNum)
+	if (acceptRange == false || realConn == 1) && !forcePiece { //need not split work
+		realConn = 1
+	}
+
+	eachPieceSize := contentLength / realConn
+	remainder = contentLength - eachPieceSize*realConn
+
+	for i := 0; i < realConn; i++ {
+		startPos = i * eachPieceSize
+		ofname = fmt.Sprintf("%s.part.%d", outputFileName, startPos)
+
+		alreadyHas := int(fileSize(ofname))
+		if alreadyHas > 0 {
+			bar.Add(alreadyHas)
+		}
+
+		//the last piece,down addtional 'remainder',eg. split 9 to 4 + (4+'1')
+		if i == realConn-1 {
+			eachPieceSize += remainder
+		}
+		Info.Printf("%s starts at %d, already has %d\n", ofname, startPos, alreadyHas)
+		go downloadOnePiece(startPos, eachPieceSize, alreadyHas, u, cookie, header, ofname)
+	}
+	return
+}
+
+func mergeChunkFiles() {
+	var n int
+	var err error
+	var chunkFiles []string
+
+	chunkFiles, err = getChunkFilesList(outputFileName)
+	if err != nil {
+		Error.Fatal("Merge chunk files failed :", err.Error())
+	}
+
+	for _, v := range chunkFiles {
+		chunkFile, _ := os.Open(v)
+		defer chunkFile.Close()
+
+		chunkReader := bufio.NewReader(chunkFile)
+		chunkWriter := bufio.NewWriter(outputFile)
+		buf := make([]byte, 100*1024*1024)
+
+		for {
+			n, err = chunkReader.Read(buf)
+			if err != nil && err != io.EOF {
+				panic(err)
+			}
+			if n == 0 {
+				break
+			}
+			if _, err = chunkWriter.Write(buf[:n]); err != nil {
+				panic(err)
+			}
+		}
+
+		if err = chunkWriter.Flush(); err != nil {
+			panic(err)
+		}
+		os.Remove(v)
+	}
+}
+
+func getContentInfomation(u string, c []Cookie, h []Header) (length int, accept bool, filename string) {
+	length = -1
+
+	resp, err := Get(u, c, h, 0, 0, 0)
+	if err != nil {
+		Error.Fatal(err.Error())
+	}
+	defer resp.Body.Close()
+
+	Info.Println("getContentInfomation resp header -->", resp.Header)
+	length, _ = strconv.Atoi(resp.Header.Get("Content-Length"))
+	if len(resp.Header.Get("Content-Range")) > 0 || len(resp.Header.Get("Accept-Ranges")) > 0 {
+		accept = true
+	}
+	filename = resp.Header.Get("filename")
+	return
+}
+
+func loadUsrDefHeaders(h []Header, req *http.Request) {
+	if len(h) == 0 {
+		return
+	}
+
+	for _, v := range h {
+		req.Header.Add(v.Header, v.Value)
+	}
+}
+
+func loadUsrDefCookies(c []Cookie, req *http.Request) {
+	if len(c) == 0 {
+		return
+	}
+
+	cookie := ""
+	for _, v := range c {
+		cookie += v.Key + "=" + v.Val + ";"
+	}
+	cookie = cookie[:len(cookie)-1] //remove the last ';'
+	req.Header.Add("Cookie", cookie)
+}
+
+func Get(path string, c []Cookie, h []Header, rangeFrom, pieceSize, alreadyHas int) (resp *http.Response, e error) {
+	var client *http.Client
+	var transport *http.Transport
+	if dialer != nil {
+		transport = &http.Transport{
+			Proxy:               nil,
+			Dial:                dialer.Dial,
+			TLSHandshakeTimeout: 30 * time.Second,
+		}
+	}
+
+	if transport != nil {
+		client = &http.Client{Transport: transport}
+	} else {
+		client = &http.Client{}
+	}
+
+	req, err := http.NewRequest("GET", path, nil)
+	if err != nil {
+		e = err
+		Error.Println(err.Error())
+		return
+	}
+
+	req.Header.Set("User-Agent", userAgent)
+	req.Header.Set("Connection", "close") // default value of HTTP1.1 is 'keep-alive'
+	if pieceSize == 0 {
+		req.Header.Add("Range", "bytes=0-")
+	} else {
+		rangeFrom += alreadyHas
+		req.Header.Add("Range", fmt.Sprintf("bytes=%d-%d", rangeFrom, rangeFrom+pieceSize-1))
+	}
+
+	loadUsrDefHeaders(h, req)
+	loadUsrDefCookies(c, req)
+
+	resp, e = client.Do(req)
+	if err != nil {
+		Error.Println(err.Error())
+	}
+
+	return
+}
+
+func createProgressBar(length int) (bar *pb.ProgressBar) {
+	bar = pb.New(length)
+	bar.ShowSpeed = true
+	bar.Units = pb.U_BYTES
+	return
+}
+
+func parseCookieLine(s []byte, host string) (c Cookie, ok bool) {
+	line := strings.Split(string(s), "\t")
+	if len(line) != 7 {
+		return
+	}
+
+	if len(line[0]) < 3 {
+		return
+	}
+
+	if line[0][0] == '#' {
+		return
+	}
+
+	domain := line[0]
+	if domain[0] == '.' {
+		domain = domain[1:]
+	}
+
+	//Curl source says, quoting Andre Garcia: "flag: A TRUE/FALSE
+	// value indicating if all machines within a given domain can
+	// access the variable.  This value is set automatically by the
+	// browser, depending on the value set for the domain."
+	//domainFlag := line[1]
+
+	//path := line[2]
+	//security := line[3]
+	expires := line[4]
+	name := line[5]
+	value := line[6]
+
+	if false == strings.Contains(host, domain) {
+		return
+	}
+
+	expiresTimestamp, err := strconv.ParseInt(expires, 10, 64)
+	if err != nil {
+		fmt.Println("expires timestamp convert error :", err)
+		return
+	}
+
+	if expiresTimestamp < time.Now().Unix() {
+		return
+	}
+
+	c.Key = name
+	c.Val = value
+	ok = true
+	return
+}
+
+func loadUsrDefinedHeader(usrDef string) (header []Header) {
+	s := strings.Split(usrDef, ";")
+	for i := 0; i < len(s); i++ {
+		l := len(s[i])
+		if l < 3 {
+			continue
+		}
+
+		kv := strings.Split(s[i], ":")
+		if len(kv) < 2 {
+			continue
+		}
+
+		//Referer:http://www.google.com
+		idx := strings.Index(s[i], ":")
+		if -1 == idx {
+			return
+		}
+		val := s[i][idx+1 : l]
+		header = append(header, Header{Header: kv[0], Value: val})
+	}
+	return
+}
+
+func loadCookies(cookiePath, host string) (cookie []Cookie, ok bool) {
+	if cookiePath == "" {
+		ok = false
+		return
+	}
+
+	f, err := os.Open(cookiePath)
+	if err != nil {
+		fmt.Println("ERROR OPEN COOKIE :", err)
+		ok = false
+		return
+	}
+	defer f.Close()
+
+	br := bufio.NewReader(f)
+	for {
+		line, isPrefix, err := br.ReadLine()
+		if err != nil && err != io.EOF {
+			ok = false
+			return
+		}
+		if isPrefix {
+			fmt.Println("you should not see this message")
+			continue
+		}
+
+		if c, ok := parseCookieLine(line, host); ok {
+			cookie = append(cookie, c)
+		}
+
+		if err == io.EOF {
+			break
+		}
+	}
+
+	ok = true
+	return
+}
+
+func downSingleFile(urlStr string) bool {
+	var err error
+	var host string
+
+	urlStr, host, err = parseUrl(urlStr)
+	if err != nil {
+		Error.Println(err.Error())
+		return false
+	}
+
+	cookies, ok := loadCookies(cookiePath, host)
+	if !ok {
+		cookies = make([]Cookie, 0)
+	}
+
+	headers := loadUsrDefinedHeader(usrDefHeader)
+
+	var headerFilename string
+	contentLength, acceptRange, headerFilename = cfgRead(urlStr)
+	if -1 == contentLength {
+		contentLength, acceptRange, headerFilename = getContentInfomation(urlStr, cookies, headers)
+
+		if "" != headerFilename {
+			outputFileName = headerFilename
+		}
+
+		if contentLength < 1 { // get failed
+			Error.Printf("Link %s get content info failed\n", urlStr)
+			getUnknownSizeFile(urlStr, cookies, headers, outputFileName)
+			return false
+		}
+
+		cfgWrite(urlStr, contentLength, acceptRange, headerFilename)
+	}
+
+	Info.Printf("[DEBUG] content length:%d,accept range:%t, cookie file:%s\n",
+		contentLength, acceptRange, cookiePath)
+
+	bar = createProgressBar(contentLength)
+	defer bar.Finish()
+
+	if outputFile, err = os.Create(outputFileName); err != nil {
+		Info.Println("error create:", outputFile, ",link:", urlStr, ",error:", err.Error(), ",name:", len(outputFileName))
+		return false
+	}
+	defer outputFile.Close()
+
+	realConn := divideAndDownload(urlStr, cookies, headers)
+	bar.Start()
+
+	allPiecesOk := true
+	for i := 0; i < realConn; i++ {
+		if !<-noticeDone {
+			fmt.Printf("Part %d is not ok!\n", i)
+			allPiecesOk = false
+		}
+	}
+
+	if !allPiecesOk {
+		return false
+	}
+
+	mergeChunkFiles()
+	cfgDelete(urlStr)
+	return true
+}
+
+func showVersionInfo() {
+	fmt.Println(fmt.Sprintf("%s Version 1.1", appName))
+	fmt.Println("Copyright (C) 2017 kumakichi")
+}
+
+func showUsage() {
+	fmt.Printf("Usage: %s [options] url1 [url2] [url...]\n", appName)
+	flag.PrintDefaults()
+}
+
+func checkUrls(u *[]string) {
+	if len(urls) == 0 {
+		if false == showVersion {
+			Error.Fatal("You must specify at least one url to download")
+		}
+	}
+
+	if len(urls) > 1 { // more than 1 url,can not set ouputfile name
+		outputFileName = defaultOutputFileName
+	}
+}
+
+func changeToOutputDir(dst string) {
+	if dst != "." {
+		if err := os.Chdir(dst); err != nil {
+			Error.Fatal("Change directory failed :", dst, err)
+		}
+	}
+}
+
+func getCookieAbsolutePath() {
+	var err error
+
+	if cookiePath == "" {
+		return
+	}
+
+	cookiePath, err = filepath.Abs(cookiePath)
+	if err != nil {
+		cookiePath = ""
+		Error.Fatal("Error get absolute path :", cookiePath)
+	}
+}
+
+func main() {
+	if len(os.Args) == 1 {
+		showUsage()
+		return
+	}
+
+	flag.Parse()
+
+	if showVersion {
+		showVersionInfo()
+	}
+
+	if len(proxyAddr) > 0 { // use sokcs
+		var err error
+		var proxyUrl *url.URL
+
+		proxyUrl, err = url.Parse(proxyAddr)
+		if err != nil {
+			Error.Printf("Failed to parse proxy URL: %v\n", err)
+		}
+
+		dialer, err = proxy.FromURL(proxyUrl, proxy.Direct)
+		if err != nil {
+			fmt.Println(err)
+			Error.Println(err.Error())
+		}
+	}
+
+	initLog()
+
+	urls = flag.Args()
+	checkUrls(&urls)
+
+	getCookieAbsolutePath()
+	changeToOutputDir(outputPath)
+
+	noticeDone = make(chan bool)
+	for i := 0; i < len(urls); i++ {
+		downSingleFile(urls[i])
+	}
+}
+
+func getUnknownSizeFile(path string, c []Cookie, h []Header, outname string) {
+	resp, err := Get(path, c, h, 0, 0, 0)
+	if err != nil {
+		Error.Println(err.Error())
+		return
+	}
+	defer resp.Body.Close()
+
+	f, err := os.OpenFile(outname, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0664)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	var n int
+	data := make([]byte, buffer_size)
+	fileOffset := 0
+
+	fmt.Println("Downloading unknown size file, please wait ...")
+	for {
+		n, err = resp.Body.Read(data)
+
+		if err != nil && err != io.EOF {
+			Error.Println(err.Error())
+			return
+		}
+
+		f.WriteAt(data[:n], int64(fileOffset))
+		fileOffset += n
+
+		if err == io.EOF {
+			fmt.Println("Done")
+			return
+		}
+	}
+}
